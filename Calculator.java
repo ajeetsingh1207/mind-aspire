5245d14edfb3b957108aae8d26e1724adcc5100b
ajeetsingh1207 <105347830+ajeetsingh1207@users.noreply.github.com>
2022-05-11 20:07:50 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,11 @@
 public class Calculator{
-  
+  public static int add(String text){
+    if(text.isEmpty()){
+      return 0;
+    } else if(text.contains(",")){
+      String[] tokens = text.split(",");
+      List<Integer> numbers = convert(token, toInt())
+      return 0;
+    }
+  }
 }
